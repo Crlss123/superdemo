d2244e0c142d17e3de89021060e275aba14ee91a
Crlss123 <camadortjr@gmail.com>
2025-11-11 14:48:28 -0700

    Initial commit

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import postRoutes from './routes/postRoutes'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/postRoutes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
-app.use('api/posts/', postRoutes)
+app.use(express.json());
+app.use(cors());
+app.use("/api/posts/", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/', (req,res) => {
+app.get("/", (req, res) => {
   res.status(201).json({
-    "hola":"hola"
-  })
-})
+    hola: "hola",
+  });
+});
