57a1db5b9c6193e4542c5e1f2c0910703bf7f16e
Crlss123 <camadortjr@gmail.com>
2025-11-11 15:07:53 -0700

    Initial commit

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/postRoutes.js";
+import initializeDatabase from "./db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+initializeDatabase();
 
 app.use(express.json());
 app.use(cors());
