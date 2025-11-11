feb55636bccffd8ea91505032ae97a2aef630939
Crlss123 <camadortjr@gmail.com>
2025-11-11 14:49:51 -0700

    Initial commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import router from "./routes/postRoutes";
+import router from "./routes/postRoutes.js";
 
 dotenv.config();
 
